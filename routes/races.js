dd3f13c4efe56df92493ae3b0e06d86c0ea9e262
JipV <jip--pie@ghotmail.com>
2015-03-13 10:09:44 +0100

    Beginnetje races

--- routes/races.js
+++ routes/races.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Race;
+var handleError;
 
-/* GET races listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+// Get all races
+function getRaces(req, res){
+	var result = Race.find({});
+	result.exec(function(err, races){
+		res.json(races);
+	});
+}
 
-module.exports = router;
+// Get race by ID
+function getRaceByID(req, res){
+	var query = { _id:req.params.id };
+	var result = Race.find(query);
+	result.exec(function(err, race){
+		if(err){ return handleError(req, res, 500, err); }
+		res.json(race);
+	});
+}
+
+// Routing
+router.route('/')
+	.get(getRaces);
+
+router.route('/:id')
+	.get(getRaceByID);
+
+// Export
+module.exports = function (mongoose, errCallback){
+	Author = mongoose.model('Race');
+	handleError = errCallback;
+	return router;
+};
