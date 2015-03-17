d6179c23619753edbf1831f4a22f6296d862e9d5
Thomas Voogt <chirimorin@gmail.com>
2015-03-17 14:16:44 +0100

    user model

--- models/user.js
+++ models/user.js
@@ -6,12 +6,37 @@ races
 visitedlocations { location: location, tijd: tijd }
  */
 
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
 function init(mongoose){
 
 	var userSchema = new mongoose.Schema({
         _id: { type: String, required: true, unique: true, lowercase: true },
-		username: { type: String, required: true },
-		password: { type: String, required: true },
+        logins: {
+            local            : {
+                email        : String,
+                password     : String
+            },
+            facebook         : {
+                id           : String,
+                token        : String,
+                email        : String,
+                name         : String
+            },
+            twitter          : {
+                id           : String,
+                token        : String,
+                displayName  : String,
+                username     : String
+            },
+            google           : {
+                id           : String,
+                token        : String,
+                email        : String,
+                name         : String
+            }
+        },
 		races: [{ type : ObjectId, ref: 'Race' }],
         visitedLocations: [{
             location: { type: ObjectId, ref: 'Location', required: true },
