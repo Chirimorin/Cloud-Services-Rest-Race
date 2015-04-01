e1aab67e204038f4e122be6be0f8056f58b84d43
JipV <jip--pie@ghotmail.com>
2015-04-01 23:03:29 +0200

    Locatie toevoegen stukje verder

--- public/js/locatiesToevoegen.js
+++ public/js/locatiesToevoegen.js
@@ -1,19 +1,25 @@
-function checkRemove() {
-    if ($('div.locatie_gegevens').length == 1) {
-        $('#remove').hide();
-    } else {
-        $('#remove').show();
-    }
-};
-
 $(document).ready(function() {
-    checkRemove();
-    $('#add').click(function() {
-        $('div.locatie_gegevens:last').after($('div.locatie_gegevens:first').clone());
-        checkRemove();
-    });
-    $('#remove').click(function() {
-        $('div.locatie_gegevens').remove();
-        checkRemove();
-    });
+    	
+	$("#btn_locatieToevoegen").on("click", function() {
+		
+		$.ajax({
+			type: "POST",
+			url: "/races/id/location?apikey=a",
+			headers: {
+				Accept: "application/json"
+			},
+			dataType: "json",
+			data: {
+				"name": $("#naam").val(),
+				"lat": $("#lat").val(),
+				"long": $("#long").val(),
+				"distance": $("#afstand").val()
+			},
+			success: function(data) {
+				
+			}
+		});	
+		
+	});
+	
 });
