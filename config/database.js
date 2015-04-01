f86e1ed30d8e9a5adaa9b97f98906447089840a5
Thomas Voogt <chirimorin@gmail.com>
2015-04-01 14:05:14 +0200

    heroku DB

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/RestRace'
-    //'url' : 'mongodb://restrace:restracepass@ds039960.mongolab.com:39960/restrace'
+    //'url' : 'mongodb://localhost:27017/RestRace'
+    'url' : 'mongodb://restrace:restracepass@ds039960.mongolab.com:39960/restrace'
 
 };
