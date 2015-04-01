412faa9263ca8edf06b835f1a43945b2e28af8fd
Thomas Voogt <chirimorin@gmail.com>
2015-04-01 12:49:17 +0200

    Races inladen

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,6 +2,10 @@
  * Created by Thomas on 31-3-2015.
  */
 
+var currentPage;
+var itemsPerPage;
+var type;
+
 function updateNickname() {
     var nickname = $("input#nickname").val();
     $.ajax({
@@ -14,6 +18,109 @@ function updateNickname() {
     });
 }
 
+function getRaces() {
+    console.log("Getting races page " + currentPage);
+
+    $.ajax({
+        url: "/races?apikey=" + apiKey +
+            "&type=" + type +
+            "&pageSize=" + itemsPerPage +
+            "&page=" + currentPage,
+        method: "GET"
+    }).success(function(data) {
+        if (data.length != 0) {
+            var raceList = "";
+
+            $.each(data, function (index, race) {
+                var startTime = new Date(race.startTime).toLocaleDateString();
+                var endTime = "";
+                if (race.endTime != null) {
+                    endTime = " - " + new Date(race.endTime).toLocaleDateString();
+                }
+
+                raceList += "<li class=\"list-group-item clickable\">" +
+                    "<span class=\"badge\">" + race.participants.length + " deelnemers" + "</span>" +
+                    "<strong>" + race.name + "</strong><br/>" +
+                    "<span class=\"tijd\">" + startTime + endTime + "</span>" +
+                    "</li>";
+            });
+
+            $("#racesList").html(raceList);
+
+            if (data.length != itemsPerPage) {
+                lastPage(true);
+            }
+            else {
+                $.ajax({
+                    url: "/races?apikey=" + apiKey +
+                    "&type=" + type +
+                    "&pageSize=" + itemsPerPage +
+                    "&page=" + (currentPage + 1),
+                    method: "GET"
+                }).success(function(data) {
+                    console.log(data);
+                    lastPage(data.length == 0)
+                });
+            }
+        }
+        else {
+            lastPage(true);
+            alert("Geen races meer gevonden.");
+        }
+    });
+
+    firstPage(currentPage == 1);
+}
+
+function previousPage() {
+    if (!$(".previous").hasClass("disabled")) {
+        currentPage--;
+        getRaces();
+    }
+}
+
+function nextPage() {
+    if (!$(".next").hasClass("disabled")) {
+        currentPage++;
+        getRaces();
+    }
+}
+
+function itemsPerPageChanged() {
+    itemsPerPage = $('#itemsPerPage').val();
+    localStorage.setItem("itemsPerPage", itemsPerPage);
+    currentPage = 1;
+    getRaces();
+}
+
+function lastPage(value) {
+    if (value) {
+        $(".next").addClass("disabled");
+    } else {
+        $(".next").removeClass("disabled");
+    }
+}
+
+function firstPage(value) {
+    if (value) {
+        $(".previous").addClass("disabled");
+    } else {
+        $(".previous").removeClass("disabled");
+    }
+}
+
 $(document).ready(function() {
+    currentPage = 1;
+    type = "owner"
+    itemsPerPage = localStorage.getItem("itemsPerPage");
+    if (!itemsPerPage) {
+        itemsPerPage = 5;
+    }
+    $('#itemsPerPage').val(itemsPerPage);
+    itemsPerPageChanged();
+
     $("#updateNickname").click(updateNickname);
+    $("#prevPage").click(previousPage);
+    $("#nextPage").click(nextPage);
+    $("#itemsPerPage").change(itemsPerPageChanged);
 });
