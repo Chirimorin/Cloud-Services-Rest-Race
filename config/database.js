bac62e796f10d183034bf3123ea68bdae35377db
Thomas Voogt <chirimorin@gmail.com>
2015-04-01 13:05:38 +0200

    database local

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    //'url' : 'mongodb://localhost:27017/RestRace' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url' : 'mongodb://restrace:restracepass@ds039960.mongolab.com:39960/restrace'
+    'url' : 'mongodb://localhost:27017/RestRace'
+    //'url' : 'mongodb://restrace:restracepass@ds039960.mongolab.com:39960/restrace'
 
 };
