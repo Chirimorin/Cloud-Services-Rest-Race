c7c43d1b2ab047270b6a625a8d2eb280c5a6b435
Thomas Voogt <chirimorin@gmail.com>
2015-04-14 11:54:36 +0200

    Andere distance berekening

--- routes/races.js
+++ routes/races.js
@@ -445,17 +445,31 @@ function addLocationToVisitedLocations(req, res) {
 }
 
 function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
-    var a =
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2)
-        ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
-    return (d * 1000);
+    //var R = 6371; // Radius of the earth in km
+    //var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    //var dLon = deg2rad(lon2-lon1);
+    //var a =
+    //        Math.sin(dLat/2) * Math.sin(dLat/2) +
+    //        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    //        Math.sin(dLon/2) * Math.sin(dLon/2)
+    //    ;
+    //var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    //var d = R * c; // Distance in km
+    //return (d * 1000);
+
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var radlon1 = Math.PI * lon1/180
+    var radlon2 = Math.PI * lon2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    dist = dist * 1609.344
+    return dist
+
 }
 
 function deg2rad(deg) {
