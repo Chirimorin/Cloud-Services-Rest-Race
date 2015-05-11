308e056c4b5e9302199bb0db438f27d9113fd588
JipV <jip--pie@ghotmail.com>
2015-05-11 20:03:49 +0200

    Database wordt leeg gegooit en user worden toegevoegd.

--- routes/data.js
+++ routes/data.js
@@ -5,9 +5,25 @@ var Race;
 var Location;
 
 function resetData(req, res, next) {
-    
+    User.remove().exec();
+ 	Race.remove().exec();
+	Location.remove().exec();
 	
+	var admin = new User();
+	admin.logins.local.email = "admin@admin.nl";
+    admin.logins.local.password = admin.generateHash("admin");
+	admin.roles.push("Admin");
+	admin.save();
 	
+	var user1 = new User();
+	user1.logins.local.email = "test@test.nl";
+    user1.logins.local.password = user1.generateHash("test");
+	user1.save();
+	
+	var user2 = new User();
+	user2.logins.local.email = "test2@test.nl";
+    user2.logins.local.password = user2.generateHash("test");
+	user2.save();
 	
 	return res.end("Database is gereset.");
 };
